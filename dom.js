011532a2691dc72bba769a5b5602e440ef526322
Rahul <115411672+Rahul-0410@users.noreply.github.com>
2022-12-18 10:50:41 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,73 @@
+// console.log("Good morning")
+// const car = {
+//     brand: "maruti",
+//     model: "swift"
+//     }
+// console.log(car.model)
+
+//  const paragraphElement=document.getElementById("paragraphTag")
+
+//  console.log(paragraphElement)
+
+// const a=document.getElementById("classTag")
+// console.log(a)
+// const b=document.getElementById("rgs")
+// b.innerHTML="Rahul Gupta"
+// console.log(b)
+
+ const a1=document.getElementById("a")
+ a1.innerHTML="Hey"
+ console.log(a1)
+
+ const b1=document.getElementById("b")
+ b1.innerHTML="my"
+ console.log(b1)
+
+ const c1=document.getElementById("c")
+ c1.innerHTML="name"
+ const d1=document.getElementById("d")
+d1.innerHTML="is"
+console.log(d1)
+
+let inputName= prompt("Enter your name:")
+const e1=document.getElementById("e")
+e1.innerHTML=inputName
+console.log(e1)
+
+const f1=document.getElementById("f")
+f1.innerHTML="and"
+console.log(f1)
+
+const g1=document.getElementById("g")
+g1.innerHTML="my"
+console.log(g1)
+
+const h1=document.getElementById("h")
+h1.innerHTML="age"
+console.log(h1)
+
+const i1=document.getElementById("i")
+i1.innerHTML="is"
+console.log(i1)
+
+let inputAge= prompt("Enter your age:")
+const j1=document.getElementById("j")
+j1.innerHTML=inputAge
+console.log(j1)
+
+const n1=document.getElementById("n")
+n1.innerHTML="and"
+console.log(n1)
+
+const k1=document.getElementById("k")
+k1.innerHTML="I"
+console.log(k1)
+
+const l1=document.getElementById("l")
+l1.innerHTML="love"
+console.log(l1)
+
+let inputsubject= prompt("Enter your favouriye subject:")
+const m1=document.getElementById("m")
+m1.innerHTML=inputsubject
+console.log(m1)
