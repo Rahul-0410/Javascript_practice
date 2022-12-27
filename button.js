4cd0cf6d22a4731148c993e2248b5b8f334c7a84
Rahul <115411672+Rahul-0410@users.noreply.github.com>
2022-12-27 09:20:59 +0530

    Add files via upload

---
+++ button.js
@@ -0,0 +1,21 @@
+let inputName=prompt("Enter your name:")
+let inputAge=prompt("Enter your age:")
+let inputGender=prompt("Enter your gender:")
+
+function showName(){
+    const nameTag=document.getElementById("name")
+    const ageTag=document.getElementById("age")
+    const genderTag=document.getElementById("gender")
+    nameTag.innerHTML=inputName
+    ageTag.innerHTML=inputAge
+    genderTag.innerHTML=inputGender
+}
+function hideName(){
+    const nameTag=document.getElementById("name")
+    const ageTag=document.getElementById("age")
+    const genderTag=document.getElementById("gender")
+    nameTag.innerHTML=""
+    ageTag.innerHTML=""
+    genderTag.innerHTML=""
+    
+}
