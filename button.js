ba1c1b46c578aa1e030db45eb67b178c1e797a58
Rahul <115411672+Rahul-0410@users.noreply.github.com>
2022-12-27 09:23:29 +0530

    Update button.js

--- button.js
+++ button.js
@@ -1,8 +1,9 @@
-let inputName=prompt("Enter your name:")
-let inputAge=prompt("Enter your age:")
-let inputGender=prompt("Enter your gender:")
+
 
 function showName(){
+    let inputName=prompt("Enter your name:")
+    let inputAge=prompt("Enter your age:")
+    let inputGender=prompt("Enter your gender:")
     const nameTag=document.getElementById("name")
     const ageTag=document.getElementById("age")
     const genderTag=document.getElementById("gender")
